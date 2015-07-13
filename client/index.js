8facfd145957de724294770877c31863cef9432e
Ionică Bizău <bizauionica@gmail.com>
2015-07-13 11:33:44 +0300

    Mad science to check the routes

--- client/index.js
+++ client/index.js
@@ -1,7 +1,19 @@
 exports.init = function () {
     this.routeNotFound = this.flow("notFound");
+    this.ruuts = [];
+    for (var i = 0; i < this._config.ruuts.length; ++i) {
+        this.ruuts.push(engine.instances[this_.config.ruuts[i]]._originalRouter);
+    }
+    global.addEventListener("popstate", this.check.bind(this, null));
 };
 
 exports.check = function () {
-    this._config.ruuts.length;
+    for (var i = 0; i < this.ruuts.length; ++i) {
+        if (this.ruuts[i](location.pathname)) {
+            return;
+        }
+    }
+    this.routeNotFound.write(null, {
+        url: location.pathname
+    });
 };
