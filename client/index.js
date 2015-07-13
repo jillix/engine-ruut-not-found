ee28edf08374cb467e0b6795037cfcfbf98a37a9
Ionică Bizău <bizauionica@gmail.com>
2015-07-13 11:39:46 +0300

    Handle routes

--- client/index.js
+++ client/index.js
@@ -2,9 +2,14 @@ exports.init = function () {
     this.routeNotFound = this.flow("notFound");
     this.ruuts = [];
     for (var i = 0; i < this._config.ruuts.length; ++i) {
-        this.ruuts.push(engine.instances[this_.config.ruuts[i]]._originalRouter);
+        var cIns = engine.instances[this._config.ruuts[i]];
+        if (!cIns) { continue; }
+        this.ruuts.push(cIns._originalRouter);
     }
     global.addEventListener("popstate", this.check.bind(this, null));
+    if (this._config.autocheck !== false) {
+        this.check();
+    }
 };
 
 exports.check = function () {
